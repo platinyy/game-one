ee5a4360fe8114ae15a5e1d9b8da20620fb8e0bd
Platin Syla <platinsyla06@gmail.com>
2023-05-04 21:30:23 -0400

    Pseudocode is added

---
+++ main.js
@@ -0,0 +1,22 @@
+/* PSEUDOCODE 
+HTML
+Define h1 with the title of the game 
+Memory card game
+Make 16 div elements each of them contanis 
+16 images , each image will match one.
+CSS
+I will use grid to layout the elemets properly 
+similar way I've done the Wirewrame.
+Javascript
+Add a let counter variable
+Store two variable for 2 similar pictures
+that we will compare later on
+Selects all the div from html
+and for each of them create a click  addEventListener.
+I will use the counter to check if any of the images has been selected yet
+I use if condition to check if  two selected pictures match each other 
+I will attach a classclicked to the element that has been clicked
+and I will change the opacity to 1 to the class to make the picture visible.
+I will add one button to start the game and 
+another button to reset and play the game again.
+*/
